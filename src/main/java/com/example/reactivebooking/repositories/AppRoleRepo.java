2ab1bd1108119b43b3fd190722c42eef8cfaaa89
Erik Svensson <eosvensson@gmail.com>
2022-03-24 21:20:39 +0100

    AppRoleRepo.findByUserRole now accepts a String instead of UserRole

--- src/main/java/com/example/reactivebooking/repositories/AppRoleRepo.java
+++ src/main/java/com/example/reactivebooking/repositories/AppRoleRepo.java
@@ -1,12 +1,11 @@
 package com.example.reactivebooking.repositories;
 
 import com.example.reactivebooking.model.AppRole;
-import com.example.reactivebooking.model.UserRole;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface AppRoleRepo extends ReactiveCrudRepository<AppRole, String> {
 
-    Mono<AppRole> findByUserRole(UserRole userRole);
+    Mono<AppRole> findByUserRole(String userRole);
 
 }
